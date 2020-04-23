75699d3d6ce365915aac57a258ef4cdd76e155d0
Decayette Evens <decayette.evens@gmail.com>
2020-04-23 11:49:05 -0700

    Create ComposeActivity and add Action

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -91,9 +92,10 @@ public class TimelineActivity extends AppCompatActivity {
         {
             //Compose item has been selected
             Toast.makeText(this,"Compose!",Toast.LENGTH_SHORT).show();
-
+            // Navigate to the compose activity
+            Intent intent = new Intent(this,ComposeActivity.class);
+            startActivity(intent);
             return  true;
-
         }
         return super.onOptionsItemSelected(item);
     }
